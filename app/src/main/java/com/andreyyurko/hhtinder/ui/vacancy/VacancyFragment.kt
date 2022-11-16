876f56437b076aa74ce1fe4308e84323e38783b9
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2022-11-16 21:18:33 +0300

    Date Picker

--- app/src/main/java/com/andreyyurko/hhtinder/ui/vacancy/VacancyFragment.kt
+++ app/src/main/java/com/andreyyurko/hhtinder/ui/vacancy/VacancyFragment.kt
@@ -1,7 +1,25 @@
 package com.andreyyurko.hhtinder.ui.vacancy
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.andreyyurko.hhtinder.R
+import com.andreyyurko.hhtinder.databinding.FragmentVacancyBinding
+import com.andreyyurko.hhtinder.utils.date.DatePicker
 
 class VacancyFragment : Fragment(R.layout.fragment_vacancy) {
+
+    private val viewBinding by viewBinding(FragmentVacancyBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initDatePicker()
+    }
+
+    fun initDatePicker() {
+        var datePicker = DatePicker(viewBinding.editTextDate, context);
+        datePicker.initDatePicker()
+    }
 }
---
+++ app/src/main/java/com/andreyyurko/hhtinder/utils/date/DatePicker.kt
@@ -0,0 +1,80 @@
+package com.andreyyurko.hhtinder.utils.date
+
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
+import android.content.Context
+import android.view.View
+import android.widget.EditText
+import java.util.*
+
+
+class DatePicker(input: EditText, context: Context?) {
+
+    private var context = context
+    private var dateButton = input
+
+    private var datePickerDialog: DatePickerDialog? = null
+
+
+    private fun getTodaysDate(): String? {
+        val cal: Calendar = Calendar.getInstance()
+        val year: Int = cal.get(Calendar.YEAR)
+        var month: Int = cal.get(Calendar.MONTH)
+        month = month + 1
+        val day: Int = cal.get(Calendar.DAY_OF_MONTH)
+        return makeDateString(day, month, year)
+    }
+
+    private fun makeDateString(day: Int, month: Int, year: Int): String? {
+        return getMonthFormat(month) + " " + day + " " + year
+    }
+
+    private fun getMonthFormat(month: Int): String {
+        if (month == 1) return "JAN"
+        if (month == 2) return "FEB"
+        if (month == 3) return "MAR"
+        if (month == 4) return "APR"
+        if (month == 5) return "MAY"
+        if (month == 6) return "JUN"
+        if (month == 7) return "JUL"
+        if (month == 8) return "AUG"
+        if (month == 9) return "SEP"
+        if (month == 10) return "OCT"
+        if (month == 11) return "NOV"
+        return if (month == 12) "DEC" else "JAN"
+
+        //default should never happen
+    }
+
+    fun initDatePicker() {
+
+        dateButton.setText(getTodaysDate())
+
+        dateButton.setOnTouchListener { view, motionEvent ->
+            openDatePicker()
+        }
+
+        val dateSetListener =
+            OnDateSetListener { datePicker, year, month, day ->
+                var month = month
+                month = month + 1
+                val date = makeDateString(day, month, year)
+                dateButton.setText(date)
+            }
+        val cal = Calendar.getInstance()
+        val year = cal[Calendar.YEAR]
+        val month = cal[Calendar.MONTH]
+        val day = cal[Calendar.DAY_OF_MONTH]
+        val style: Int = AlertDialog.THEME_HOLO_LIGHT
+        datePickerDialog = DatePickerDialog(context!!, style, dateSetListener, year, month, day)
+    }
+
+    fun openDatePicker(): Boolean {
+        if (datePickerDialog != null) {
+            datePickerDialog!!.show()
+        }
+
+        return true
+    }
+}
